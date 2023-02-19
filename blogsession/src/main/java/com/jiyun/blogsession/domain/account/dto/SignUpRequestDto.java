bce6903a470436f0a321927d1f2be3aa5391a227
june0216 <ziyun1612@ewhain.net>
2023-02-19 12:36:01 +0900

    feat : Account api - Account 탈퇴(삭제, 수정 버전 둘 다), 정보 수정 추가

--- blogsession/src/main/java/com/jiyun/blogsession/domain/account/dto/SignUpRequestDto.java
+++ blogsession/src/main/java/com/jiyun/blogsession/domain/account/dto/SignUpRequestDto.java
@@ -1,42 +1,46 @@
 package com.jiyun.blogsession.domain.account.dto;
 
 import com.jiyun.blogsession.domain.account.domain.Account;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class SignUpRequestDto {
-
-	//@NotEmpty, @Length, @Email은 validation이므로 어떤 데이터 (주로 사용자 또는 다른 서버의 request)의 값이 유효한지, 잘못된 내용이 있는지 확인하는 단계
-	@NotBlank(message = "이메일은 필수입니다.") //@NotBlank 는 null 과 "" 과 " " 모두 허용하지 않는다.
+	@NotBlank(message = "이메일은 필수입니다.")
 	@Email(message = "유효하지 않은 이메일 형식입니다.",
 			regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
 	private String email;
 
 	@NotBlank(message = "비밀번호는 필수입니다.")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!.?,])[A-Za-z\\d!.?,]{2,16}$",
+			message = "16자 이내의 영문자 및 숫자와 ?,!,., , 특수문자로 입력해주세요.")
 	private String password;
 
 	@NotBlank(message = "닉네임은 필수입니다. ")
-	@Column(nullable = false, length = 16)
 	private String nickname;
 
 	@Builder
-	public SignUpRequestDto(String email, String nickname, String password) {
+	public SignUpRequestDto(String email, String password, String nickname) {
 		this.email = email;
-		this.nickname = nickname;
 		this.password = password;
+		this.nickname = nickname;
 	}
 
 	public Account toEntity() {
 		return Account.builder()
 				.email(this.email)
-				.encodedPassword(this.password)
+				.password(this.password)
 				.nickname(this.nickname)
+				.bio("안녕하세요!")
 				.build();
 	}
 
