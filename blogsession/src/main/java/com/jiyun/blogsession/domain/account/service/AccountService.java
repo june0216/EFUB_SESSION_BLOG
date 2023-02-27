a4aa2ef970c4f227a7bcc46818f34734d26ab3f4
june0216 <ziyun1612@ewhain.net>
2023-02-28 01:31:57 +0900

    fix : Account STATUS 기본값 설정 해제

--- blogsession/src/main/java/com/jiyun/blogsession/domain/account/service/AccountService.java
+++ blogsession/src/main/java/com/jiyun/blogsession/domain/account/service/AccountService.java
@@ -5,16 +5,12 @@ import com.jiyun.blogsession.domain.account.dto.AccountUpdateRequestDto;
 import com.jiyun.blogsession.domain.account.dto.SignUpRequestDto;
 import com.jiyun.blogsession.domain.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
-
-@Slf4j
 @Service//서비스 레이어, 내부에서 자바 로직을 처리함
-@Transactional
 @RequiredArgsConstructor //final 키워드가 붙은 필드에 대해 생성자 자동 생성
 public class AccountService {
 	private final AccountRepository accountRepository;
@@ -22,7 +18,7 @@ public class AccountService {
 
 	public Long signUp(SignUpRequestDto requestDto){
 		if (isExistedEmail(requestDto.getEmail())){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("이미 존재하는 email입니다. " + requestDto.getEmail());
 		}
 		Account account = accountRepository.save(requestDto.toEntity());
 		return account.getAccountId();
@@ -47,13 +43,13 @@ public class AccountService {
 		account.withdrawAccount();
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional(readOnly =true)
 	public Account findById(Long id) {
 		return accountRepository.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException("해당 id 를 가진 Account 를 찾을 수 없습니다. id ="+id));
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional(readOnly =true)
 	public boolean isExistedEmail(String email){
 		return accountRepository.existsByEmail(email);
 	}
