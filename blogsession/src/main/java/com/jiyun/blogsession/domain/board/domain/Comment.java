d03425bfcebbfe16ebd0cc522f6eca91123ae63a
june0216 <ziyun1612@ewhain.net>
2023-03-25 02:17:34 +0900

    fix : 오타 수정

--- blogsession/src/main/java/com/jiyun/blogsession/domain/board/domain/Comment.java
+++ blogsession/src/main/java/com/jiyun/blogsession/domain/board/domain/Comment.java
@@ -13,7 +13,6 @@ import javax.persistence.*;
 
 @Entity//해당 클래스에 있는 내부변수에 모두 @Column을 내부적으로 포함 -> 옵셥없으면 생략 가능
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자의 접근 제어를 PROTECTED로 설정해놓게 되면 무분별한 객체 생성에 대해 한번 더 체크할 수 있는 수단
-@DynamicInsert//status 기본값 유지를 위해
 @Getter
 public class Comment extends BaseTimeEntity {
 	@Id
